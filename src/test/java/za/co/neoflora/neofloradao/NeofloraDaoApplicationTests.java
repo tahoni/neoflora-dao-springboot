78de9ed43464d13b7a55a4e2bb140bbf1060168d
Leoni Lubbinge <tahoni@gmail.com>
2022-07-20 01:23:47 +0200

    Initial commit

---
+++ src/test/java/za/co/neoflora/neofloradao/NeofloraDaoApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.neoflora.neofloradao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeofloraDaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
